60a0f4c531fcb80d0eabd6d6b5a9e716675f6faa
mlk <malik@kone.ci>
2025-05-18 16:02:56 +0000

    adding a simple reset_script

---
+++ reset_dev_db.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+docker compose -f docker-compose-dev.yml exec web bash -c \
+  "python manage.py reset_db && \
+   python manage.py migrate --noinput && \
+   python manage.py populate_initial_data"
