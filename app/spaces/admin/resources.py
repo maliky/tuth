9bdfe9815de85d832d117965ca4bfbf3ad3ca871
mlk <malik@kone.ci>
2025-05-22 10:27:17 +0000

    refactoring naming classes

---
+++ app/spaces/admin/resources.py
@@ -0,0 +1,17 @@
+from import_export import fields, resources
+from space.models import Room
+
+from .widgets import BuildingWidget
+
+
+class RoomResource(resources.ModelResource):
+    building = fields.Field(
+        column_name="building",
+        attribute="building",
+        widget=BuildingWidget("building", "short_name"),
+    )
+
+    class Meta:
+        model = Room
+        import_id_fields = ("name", "building")
+        fields = ("name", "building")
