2333d7a050368d9e5d65155bba1bcd889efe472b
mlk <malik@kone.ci>
2025-05-29 16:05:01 +0000

    website

--- app/settings.py
+++ app/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # my apps
+    "app.website",
     "app.academics",
     "app.finance",
     "app.people",
