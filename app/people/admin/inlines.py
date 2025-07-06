4af3d02913a09b78e1d6bb096394b459ec227787
mlk <malik@kone.ci>
2025-07-06 16:28:21 +0000

    working on harmonizing users

---
+++ app/people/admin/inlines.py
@@ -0,0 +1,11 @@
+"""Inlines forms for the admin interface of people."""
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserInline(admin.StackedInline):
+    model = User
+    form = UserCreationForm
+    can_delete = False
+    verbose_name_plural = 'User'
+    fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
