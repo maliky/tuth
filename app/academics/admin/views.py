b36086613e8cc1ac7f5c15985a98ae0f72f7cf07
mlk <malik@kone.ci>
2025-07-18 17:26:07 +0000

    trying to dropable searchable filters for curriculum and semester

---
+++ app/academics/admin/views.py
@@ -0,0 +1,24 @@
+"""Views for the academics admin."""
+
+from admin_searchable_dropdown.views import AutocompleteJsonView
+from app.academics.models import Curriculum
+
+class CurriBySemAc(AutocompleteJsonView):
+    """Returns the curriculums with section offered during a specific semester."""
+
+    model_admin = None
+
+    @staticmethod
+    def display_text(obj):
+        """Set the text to show in the dropdown box."""
+        return f"{obj}"
+
+    def get_queryset(self):
+        """Returns the filter queryset using semeter id passed in the url."""
+
+        semester_id = self.request.GET.get("semester")
+        if not semester_id:
+            return Curriculum.objects.all()
+
+        return Curriculum.objects.filter(programs__sections__semester_id = semester_id)
+
