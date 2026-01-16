569e8535bf907abf582b4ef5aad0b6badce414ab
mlk@r1d05 <mkone@tubmanu.edu.lr>
2026-01-16 11:24:25 +0000

    updated filters in section

--- app/timetable/admin/section_registers.py
+++ app/timetable/admin/section_registers.py
@@ -4,16 +4,12 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 
-from app.academics.admin.filters import DepartmentFilterAC
+from app.academics.admin.filters import CurriculumFilterAC
 from app.people.models.faculty import Faculty
 from app.registry.admin.inlines import GradeInline
 from app.shared.admin.filters import BaseCollegeFilter
 from app.shared.admin.mixins import CollegeRestrictedAdmin
-from app.timetable.admin.filters import (
-    SectionCollegeFilter,
-    SectionFacultyFilterAc,
-    SemesterFilterAC,
-)
+from app.timetable.admin.filters import SectionFacultyFilterAc, SemesterFilterAC
 from app.timetable.admin.inlines import SecSessionInline
 from app.timetable.admin.section_resources import SectionResource
 from app.timetable.models.section import Section
@@ -39,14 +35,16 @@ class SectionAdmin(CollegeRestrictedAdmin):
         "space_codes",
         "session_count",
         "credit_hours",
-        "curriculum_course__curriculum",
+        "curriculum_display",
     )
+    # need to be a field of the section
+    # list_editable = ("curriculum_course__curriculum",)
     inlines = [SecSessionInline, GradeInline]
+    # Added curriculum filtering per section list requirements.
     list_filter = [
         SectionFacultyFilterAc,
-        DepartmentFilterAC,
+        CurriculumFilterAC,
         SemesterFilterAC,
-        SectionCollegeFilter,
     ]
     autocomplete_fields = (
         "semester",
@@ -54,7 +52,12 @@ class SectionAdmin(CollegeRestrictedAdmin):
     )
 
     # Join related tables to reduce queries when pulling sections.
-    list_select_related = ("curriculum_course", "semester", "faculty")
+    list_select_related = (
+        "curriculum_course",
+        "curriculum_course__curriculum",
+        "semester",
+        "faculty",
+    )
 
     search_fields = (
         "^curriculum_course__course__short_code",
@@ -102,6 +105,11 @@ class SectionAdmin(CollegeRestrictedAdmin):
     def credit_hours(self, obj: Section) -> int:
         """Return credit hours for this section's curriculum_course."""
         return obj.curriculum_course.credit_hours_id or 3
+
+    @admin.display(description="Curriculum", ordering="curriculum_course__curriculum")
+    def curriculum_display(self, obj: Section) -> str:
+        """Return the curriculum name for the section listing."""
+        return str(obj.curriculum_course.curriculum)
 
     @admin.display(description="Faculty", ordering="faculty")
     def faculty_link(self, obj: Section) -> str:
